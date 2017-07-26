da0b183e4cb2cbcbefda72deca4e34d0991cfc48
dogoncouch <dpersonsdev@gmail.com>
2017-07-26 19:52:56 -0400

    Add site layout test for logged in users

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,12 +2,28 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
-  test "layout links" do
+  def setup
+    @user = users(:michael)
+  end
+
+  test "layout links for non-logged-in user" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+  end
+
+  test "layout links for logged-in user" do
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", users_path
+    assert_select "li[class=dropdown]"
   end
 end
